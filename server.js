4bf4f7373f28fff904b7d891a926999d788d0ec3
AravindBalaji1707 <balarvy@gmail.com>
2016-09-19 11:27:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req, res) {
+  res.send('Article one');
+});
+app.get('/article-two', function (req, res) {
+  res.send('Article 2');
+});
+app.get('/article-three', function (req, res) {
+  res.send('Article 3 ');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
